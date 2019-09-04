cc3a4f92db2c69ee5fb8f8053efc68d5e3ae8714
Simone <s.maya.cunningham@gmail.com>
2019-09-03 21:49:16 -0700

    update

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,8 +1,10 @@
 // array of words
-const words = ["Dead", "zombies", "Walking", "evolution", "Omega"]
+const words = ['Dead', 'zombies', 'Walking', 'evolution', 'Rick', 'mercy', 'monster', 'Glenn', 'Maggie', 'run'];
 
 // how do you make underscores appear under words based on length 
 //random word generator
+let chosenWord = Math.floor(Math.random() * words.length);
+console.log(chosenWord);
 //retrieve users guess and check to see if the guess is correct 
 // if correct push to right array? 
 // if wrong push to wrong array. 
